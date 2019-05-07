39f1303a1ae44b4037b09d575ce3648aba3359c0
prashanth-sams <sams.prashanth@gmail.com>
2019-05-07 18:13:15 +0400

    minor fixes

--- Adoptdemoapptest.py
+++ Adoptdemoapptest.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver import ChromeOptions
+import unittest
+import os
 
+remote_url='http://localhost:4444/wd/hub'
+driver = webdriver.Remote(command_executor=remote_url,desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+driver.implicitly_wait(30)
+driver.maximize_window()
 
-driver = webdriver.Chrome()
-chrome = webdriver.Remote(
-          command_executor='http://localhost:4444/wd/hub',
-          desired_capabilities=DesiredCapabilities.CHROME)
-chrome.get ('http://localhost:9005/')
-print(chrome.title)
+driver.get ('http://localhost:9005/')
+print(driver.title)
+driver.close()
+driver.quit()
